9a708d30dad02b37dbddfd8aaf3517e2c2e09f92
WaterTian <changewater@qq.com>
2021-01-18 19:41:05 +0800

    create  scene3

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 _mouse = new Vector3(Input.mousePosition.x / Screen.width - 0.5f, Input.mousePosition.y / Screen.height - 0.5f, 0);
+        var vfx = GetComponent<UnityEngine.VFX.VisualEffect>();
+        vfx.SetVector3("MousePostion", _mouse);
+
+    }
+}
